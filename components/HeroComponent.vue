89260074308ef022f07cdd23696a0a0b57cb56fb
Dennis F <farkasdenn@gmail.com>
2024-04-05 23:44:35 +0300

    cms integration + adjustments

--- components/HeroComponent.vue
+++ components/HeroComponent.vue
@@ -2,13 +2,13 @@
   <div class="flex gap-12 flex-col pt-8 md:pt-12 md:w-[602px] xl:flex-row xl:w-[1224px] xl:pt-0">
     <div class="text-white text-center gap-6 flex flex-col items-center md:gap-10 xl:inline-flex xl:items-start xl:text-left xl:w-[570px] xl:pt-[100px]">
       <h1 class="text-[40px] md:text-[60px] md:w-[567px] font-bold leading-[normal]">
-        Complete <span class="text-green">Vue.js training</span> solutions for companies
+        {{firstWord}}<span class="text-green">{{' ' + coloredText + ' '}}</span>{{lastTextStrig}}
       </h1>
       <p class="font-normal leading-6 md:text-[22px] md:w-[570px] md:leading-[30px] text-center">
-        Training solutions designed for companies, agencies and organisations with developers using or who are considering using the Vue.js framework
+        {{heroText}}
       </p>
       <ButtonComponent
-          btn-name="Talk to Sales"
+          :btn-name="btnText"
       />
     </div>
     <PeopleImage class="md:w-[602px] xl:pt-8"/>
@@ -17,6 +17,29 @@
 
 <script setup>
 import PeopleImage from '../public/assets/people-image.svg'
+
+const props = defineProps({
+  heroText: {
+    type: String,
+    required: true
+  },
+  heroTitle: {
+    type: String,
+    required: true
+  },
+  btnText: {
+    type: String,
+    required: true
+  }
+});
+
+const wordsArray = props.heroTitle.split(' ');
+const firstWord = wordsArray[0]
+const secondWord = wordsArray[1];
+const thirdWord = wordsArray[2];
+const coloredText = secondWord + ' ' + thirdWord;
+wordsArray.splice(0, 3);
+const lastTextStrig = wordsArray.join(' ')
 </script>
 
 <style scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="w-[345px] md:w-[688px] xl:w-[1170px] flex flex-col items-center">
-    <HeroComponent/>
+    <HeroComponent
+        :hero-title="data[0].heroTitle"
+        :hero-text="data[0].heroText"
+        :btn-text="data[0].btnText"
+    />
     <InfoSection/>
     <Google class="max-xl:hidden"/>
-    <Statistics/>
+    <Statistics
+        :videos="data[0].videoLessons"
+        :courses="data[0].courses"
+        :hours="data[0].hours"
+    />
     <div class="w-[345px] flex items-center mt-[120px] md:mt-[140px] md:w-[688px] xl:w-[752px] xl:mt-[200px]">
       <h1 class="text-white font-bold text-[40px] leading-[normal] text-center md:text-[60px]">
         Discounted <span class="text-transparent bg-clip-text bg-gradient-to-r from-green to-[#00F0FF]">Corporate Training</span> Bundles
       </h1>
     </div>
     <div class="flex flex-col items-start gap-6 self-stretch mt-[56px] md:mt-[72px] xl:mt-[120px] xl:flex-row">
-      <Card v-for="(item, key) in cardObj"
+      <Card v-for="(item, key) in data[0].cards"
             :title="item.title"
             :icon-name="item.icon"
             :card-obj="item.options"
@@ -19,48 +27,34 @@
     </div>
     <TrainingComponent/>
     <FooterComponent/>
-    <div class="h-40"></div>
   </div>
 </template>
 
 <script setup>
 import TrainingComponent from "~/components/TrainingComponent.vue";
 
-const cardObj = [
-  {
-    title: 'Basic',
-    icon: 'leaf',
-    options: [
-      {checkbox: true, title: 'All Video Courses', textBold: false},
-      {checkbox: true, title: 'Vue.Js Master class', textBold: false},
-      {checkbox: true, title: 'Developer assist Slack channel', textBold: false},
-      {checkbox: false, title: 'Live Weekly QnA', textBold: false},
-      {checkbox: false, title: '1 x ws ticket per license', textBold: false}
-    ]
-  },
-  {
-    title: 'Professional',
-    icon: 'rocket',
-    options: [
-      {checkbox: true, title: 'All Video Courses', textBold: false},
-      {checkbox: true, title: 'Vue.Js Master class', textBold: false},
-      {checkbox: true, title: 'Developer assist Slack channel', textBold: false},
-      {checkbox: false, title: 'Live Weekly QnA', textBold: true},
-      {checkbox: false, title: '1 x ws ticket per license', textBold: true}
-    ]
-  },
-  {
-    title: 'Basic',
-    icon: 'bolt',
-    options: [
-      {checkbox: true, title: 'All Video Courses', textBold: false},
-      {checkbox: true, title: 'Vue.Js Master class', textBold: false},
-      {checkbox: true, title: 'Developer assist Slack channel', textBold: false},
-      {checkbox: false, title: 'Live Weekly QnA', textBold: false},
-      {checkbox: false, title: '4 x Virtual Workshop of your choice', textBold: true}
-    ]
-  },
-];
+const query = groq`*[_type == 'home']{
+heroTitle,
+courses,
+cards[]{
+  icon,
+  title,
+  options[]{
+    checkbox,
+    title,
+    textBold
+  }
+},
+videoLessons,
+hours,
+btnText,
+heroText
+}`
+
+const sanity = useSanity()
+const { data } = await useAsyncData('cards', () => sanity.fetch(query))
+
+console.log(data)
 </script>
 
 <style>
